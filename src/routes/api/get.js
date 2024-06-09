ece58a37ca70d6a39e551432cc185004f3485b5c
Armen M <amerzanian@myseneca.ca>
2024-06-09 18:41:56 -0400

    Update GET /fragments to return a list of the auth'd user fragment IDs

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -1,15 +1,22 @@
 // src/routes/api/get.js
 
-const { createSuccessResponse } = require("../../response");
-
+const { createSuccessResponse, createErrorResponse } = require("../../response");
+const { Fragment } = require('../../model/fragment');
 const logger = require('../../logger');
 
 /**
  * Get a list of fragments for the current user
  */
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   logger.info('GET /v1/fragments called')
-  res.status(200).json(createSuccessResponse({
-    fragments: [],
-  }));
+  try {
+    const userId = req.user; // Authenticated user's ID
+    const fragments = await Fragment.byUser(userId);
+    
+    logger.info(`Returning ${fragments.length} fragments for user ${userId}`);
+    res.status(200).json(createSuccessResponse({ fragments }));
+  } catch (err) {
+    logger.error({ err }, 'Error retrieving fragments');
+    res.status(500).json(createErrorResponse(500, 'Unable to retrieve fragments'));
+  }
 };
